1df977bfc952f52b92d5618493f8ae0b2a985308
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-02-06 22:34:00 +0100

    ✨ <refactor>[Main] Modify main function to use MainMenu instead of TerminalDisplay; comment out TerminalDisplay initialization

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,15 @@
 #include "include/TerminalDisplay.hpp"
+#include "MainMenu.ui.hpp"
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
-
+MainMenu mainMenu;
 int main()
 {
-    TerminalDisplay terminalDisplay;
-    terminalDisplay.start();
+    // TerminalDisplay terminalDisplay;
+    // terminalDisplay.start();
+    mainMenu.mainMenu();
+    return 0;
 }
