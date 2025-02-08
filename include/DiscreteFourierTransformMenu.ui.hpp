c2df0405995beef0cd715ec7aca01a8636e84de6
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-02-08 14:56:06 +0100

    ✨ <refactor>[DiscreteFourierTransform] Enhance documentation comments for DiscreteFourierTransform and DiscreteFourierTransformMenu classes

--- include/DiscreteFourierTransformMenu.ui.hpp
+++ include/DiscreteFourierTransformMenu.ui.hpp
@@ -3,9 +3,21 @@
 
 #include <opencv2/opencv.hpp>
 #include "DiscreteFourierTransform.hpp"
-
+/**
+ * @class DiscreteFourierTransformMenu
+ * @brief The DiscreteFourierTransformMenu class
+ * This class is used to display the menu for the Discrete Fourier Transform
+ * operations.
+ */
 class DiscreteFourierTransformMenu {
 public:
+    /**
+     * @class DiscreteFourierTransformMenu
+     * @brief displayDFTMenu
+     * This function displays the menu for the Discrete Fourier Transform
+     * operations.
+     * @param image The image on which the operations are to be performed.
+     */
     static void displayDFTMenu(cv::Mat& image);
 };
 
