372067cdd4e4080f4b99dc4c572eeca502ba1eb8
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-02-25 01:13:43 +0100

    ✨ <feat>[DiscreteFourierTransform] Enhance DFT computation with multi-threading support and add benchmarking functionality.

--- include/DiscreteFourierTransform.hpp
+++ include/DiscreteFourierTransform.hpp
@@ -2,79 +2,88 @@
 #define DISCRETEFOURIERTRANSFORM_HPP
 
 #include <opencv2/opencv.hpp>
+#include "ThreadManager.hpp"
+
 /**
-* @class DiscreteFourierTransform
-* @brief Class to perform Discrete Fourier Transform on an image.
-* @return The resulting image after applying Discrete Fourier Transform.
-*/
+ * @class DiscreteFourierTransform
+ * @brief Class to perform Discrete Fourier Transform on an image.
+ */
 class DiscreteFourierTransform {
 public:
     /**
-     * @class DiscreteFourierTransform
-     * @brief Default constructor.
-     * @brief Load the image from the given filename.
-     * @param filename The name of the image file.
-     * @return True if the image is loaded successfully, false otherwise.
+     * @brief Constructor to initialize the class with an image.
+     * @param image The input image to be processed.
      */
     explicit DiscreteFourierTransform(const cv::Mat& image);
+
     /**
-     * @class DiscreteFourierTransform
      * @brief Destructor of DiscreteFourierTransform class.
      */
     ~DiscreteFourierTransform();
+
     /**
-     * @class DiscreteFourierTransform
      * @brief Apply Discrete Fourier Transform to the image.
-    */
-    void computeDFT();
+     * @param mode The threading mode to use (SINGLE_THREAD, MULTI_THREAD, MULTI_THREAD_MUTEX).
+     * @param numThreads The number of threads to use.
+     */
+    void computeDFT(ThreadManager::Mode mode, int numThreads);
+
     /**
-     * @class DiscreteFourierTransform
      * @brief Compute the magnitude spectrum of the image.
      */
     void computeMagnitudeSpectrum();
+
     /**
-     * @class DiscreteFourierTransform
      * @brief Rotate the image by the given angle using Discrete Fourier Transform.
      * @param angle The angle by which the image is to be rotated.
      * @return The rotated image.
      */
     cv::Mat rotateImageUsingDFT(double angle);
-    void showImgResult();
+
     /**
-     * @class DiscreteFourierTransform
-     * @brief Display the magnitude spectrum of the image and the image.
+     * @brief Display the magnitude spectrum of the image and the original image.
      */
     void showResult();
-        /**
-     * @class DiscreteFourierTransform
-     * @brief Display the magnitude spectrum of the image.
-     * @param complexImage The complex image.
+
+    /**
+     * @brief Rotate the complex image by the given angle using Discrete Fourier Transform.
+     * @param complexImage The complex image to be rotated.
      * @param angle The angle by which the image is to be rotated.
-     * @return rotatedComplexImage The rotated complex image.
+     * @return The rotated complex image.
      */
     cv::Mat rotateDFT(const cv::Mat& complexImage, double angle);
-        /**
-     * @class DiscreteFourierTransform
-     * @brief Compute the inverse Discrete Fourier Transform of the complexImage.
+
+    /**
+     * @brief Compute the inverse Discrete Fourier Transform of the complex image.
      * @param complexImage The complex image to be processed.
-     * @return The inverse Discrete Fourier Transform of the complexImage.
+     * @return The inverse Discrete Fourier Transform of the complex image.
      */
     cv::Mat inverseDFT(const cv::Mat& complexImage);
+
     /**
-     * @class DiscreteFourierTransform
      * @brief Display the rotated image.
      */
     void showRotatedImage();
+
     /**
-     * @class DiscreteFourierTransform
      * @brief Print the matrices of the image.
      */
     void printMatrices();
 
-private:
-    cv::Mat img, padded, complexImage, magnitudeSpectrum, rotatedImage;
     /**
-     * @class DiscreteFourierTransform
+     * @brief Benchmark the processing speed of different modes.
+     */
+    void benchmarkProcessing();
+
+private:
+    cv::Mat imgInput; ///< The input image.
+    cv::Mat img; ///< The image to be processed.
+    cv::Mat padded; ///< The padded image.
+    cv::Mat complexImage; ///< The complex image after DFT.
+    cv::Mat magnitudeSpectrum; ///< The magnitude spectrum of the image.
+    cv::Mat rotatedImage; ///< The rotated image.
+
+    /**
      * @brief Rearrange the quadrants of the image.
      * 
      * The quadrants are rearranged so that the origin is at the center of the image.
@@ -91,6 +100,14 @@ private:
      * since the origin is at the center of the image.
      */
     void rearrangeQuadrants();
+
+    /**
+     * @brief Perform DFT processing on a portion of the image.
+     * @param image The image to be processed.
+     * @param yStart The starting row index.
+     * @param yEnd The ending row index.
+     */
+    static void dftProcessing(cv::Mat& image, int yStart, int yEnd);
 };
 
 #endif // DISCRETEFOURIERTRANSFORM_HPP
